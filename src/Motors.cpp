bf028632e6053dd0750ffa7c91e15aab2a619556
Supun Gamlath <supungamlath@outlook.com>
2022-10-18 10:10:24 +0530

    Use micros to set step frequency

--- src/Motors.cpp
+++ src/Motors.cpp
@@ -3,10 +3,8 @@
 #include <Types.h>
 #include <PWM.h>
 
-// const int maxSpeedStepDelay = 30;
-// const int minSpeedStepDelay = 1500;
-const int minFrequency = 500;
-const int maxFrequency = 10000;
+const int maxSpeedStepDelay = 30;
+const int minSpeedStepDelay = 1500;
 
 void setMotorsDirection(RotationDirection direction)
 {
@@ -18,9 +16,6 @@ void setMotorsDirection(RotationDirection direction)
 
 void initializeMotors()
 {
-    // Initialize all timers except for 0, to save time keeping functions
-    InitTimers();
-
     // Sets the pins as Outputs
     for (int motor = 0; motor < 3; motor++)
     {
@@ -32,21 +27,14 @@ void initializeMotors()
 
 void runMotors(int speed)
 {
-    // int stepDelay = map(speed, 0, 1023, minSpeedStepDelay, maxSpeedStepDelay);
-    int frequency = map(speed, 0, 1023, minFrequency, maxFrequency);
-    SetPinFrequency(stepPins[1], frequency);
-    SetPinFrequency(stepPins[2], frequency);
+    int stepDelay = map(speed, 0, 1023, minSpeedStepDelay, maxSpeedStepDelay);
     for (int motor = 0; motor < 3; motor++)
     {
         // Serial.println(stepDelay);
-        // digitalWrite(stepPins[motor], HIGH);
+        digitalWrite(stepPins[motor], HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPins[motor], LOW);
         // delayMicroseconds(stepDelay);
-        // digitalWrite(stepPins[motor], LOW);
-        // delayMicroseconds(stepDelay);
-
-        // Set frequency of PWM pins
-        // use this functions instead of analogWrite on 'initialized' pins
-        pwmWrite(stepPins[motor], 127);
     }
 }
 
@@ -54,7 +42,6 @@ void stopMotors()
 {
     for (int motor = 0; motor < 3; motor++)
     {
-        pwmWrite(stepPins[motor], 0);
-        // digitalWrite(stepPins[motor], LOW);
+        digitalWrite(stepPins[motor], LOW);
     }
 }
