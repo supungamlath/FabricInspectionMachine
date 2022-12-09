e4618bb6f2184b8a2c5cfbf2d867bb6850346fc9
Supun Gamlath <supungamlath@outlook.com>
2022-12-09 09:16:01 +0530

    Seperate Arduino Uno for motor control

--- src/Motors.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void initializeMotors();
-void runMotors(int speed);
-void stopMotors();
-void setMotorsDirection(RotationDirection direction);
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <Arduino.h>
 #include <Types.h>
-#include <Motors.h>
+#include <MotorsTX.h>
 #include <Joystick.h>
 #include <SpeedController.h>
 #include <Encoder.h>
@@ -14,11 +14,17 @@ void setup()
   initializeJoystick();
   initializeSwitch();
   initializeSpeedController();
+<<<<<<< Updated upstream
   // Serial.begin(115200);
+=======
+  initializeBluetooth();
+  Serial.begin(115200);
+>>>>>>> Stashed changes
 }
 
 void loop()
 {
+<<<<<<< Updated upstream
   // checkRotaryWithDebouncing();
   ControllerPosition joystickPosition = getJoystickXPosition();
   ControllerPosition switchPosition = getSwitchPosition();
@@ -43,5 +49,42 @@ void loop()
   else
   {
     stopMotors();
+=======
+  ControllerPosition switchPosition = getSwitchPosition();
+  // // Value from 0 to 1023
+  if (switchPosition == MIDDLE)
+  {
+    ControllerPosition joystickPosition = getJoystickXPosition();
+    Serial.print(" Joystick ");
+    Serial.println(joystickPosition);
+    // int encoderReading = getEncoderReading();
+    if (joystickPosition == LEFT)
+    {
+      setMotorsDirection(FORWARD);
+      runMotors(400);
+    }
+    else if (joystickPosition == RIGHT)
+    {
+      setMotorsDirection(BACKWARD);
+      runMotors(400);
+    }
+    stopMotors();
+  }
+  else
+  {
+    int speed = getSpeedControllerReading();
+    Serial.print("Speed ");
+    Serial.print(speed);
+    if (switchPosition == LEFT)
+    {
+      setMotorsDirection(BACKWARD);
+      runMotors(speed);
+    }
+    else if (switchPosition == RIGHT)
+    {
+      setMotorsDirection(FORWARD);
+      runMotors(speed);
+    }
+>>>>>>> Stashed changes
   }
 }
